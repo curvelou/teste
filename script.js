3d1a42a569e2b8b6f906a8a5185744e9176a9913
curvelou <145216789+curvelou@users.noreply.github.com>
2023-09-16 19:13:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -34,8 +34,6 @@ function displayData() {
         `;
         dataDisplay.appendChild(dataItem);
     });
-
-    generateChart();
 }
 
 function clearForm() {
@@ -43,53 +41,7 @@ function clearForm() {
     dataForm.reset();
 }
 
-function generateChart() {
-    let chartType = document.getElementById('chartType').value;
-    let dataToPlot = document.getElementById('dataToPlot').value;
-
-    let labels = [];
-    let data = [];
-
-    dataStorage.forEach((item, index) => {
-        labels.push('Ponto ' + (index + 1));
-        data.push(item[dataToPlot]);
-    });
-
-    let ctx = document.getElementById('dataChart').getContext('2d');
-    if (window.dataChart) {
-        window.dataChart.destroy();
-    }
-
-    window.dataChart = new Chart(ctx, {
-        type: chartType,
-        data: {
-            labels: labels,
-            datasets: [{
-                label: dataToPlot,
-                data: data,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-}
-
 function downloadData() {
     let blob = new Blob([JSON.stringify(dataStorage)], { type: 'application/json' });
     saveAs(blob, 'data.json');
-}
-
-function downloadChart() {
-    let canvas = document.getElementById('dataChart');
-    canvas.toBlob(function (blob) {
-        saveAs(blob, 'chart.png');
-    });
 }
