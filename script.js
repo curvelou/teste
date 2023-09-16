bd89b1b928725f73911dae8948eb1f6bdb55ef33
curvelou <145216789+curvelou@users.noreply.github.com>
2023-09-16 14:42:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,120 +1,30 @@
-let dataStorage = [];
+let dataStorage1 = [];
+let map;
+let markerGroup;
 
-function saveData() {
-    let dataForm = document.getElementById('dataForm');
+function initMap() {
+    map = L.map('map').setView([0, 0], 2);
+    markerGroup = L.layerGroup().addTo(map);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
+
+function saveData1() {
+    let dataForm1 = document.getElementById('dataForm1');
     let data = {
-        point: dataForm.point.value,
-        latitude: dataForm.latitude.value,
-        longitude: dataForm.longitude.value,
-        tideCondition: dataForm.tideCondition.value,
-        waterCondition: dataForm.waterCondition.value,
-        fragmentQuantity: parseInt(dataForm.fragmentQuantity.value),
-        fiberQuantity: parseInt(dataForm.fiberQuantity.value),
-        colors: {
-            blue: parseInt(dataForm.blue.value),
-            red: parseInt(dataForm.red.value),
-            green: parseInt(dataForm.green.value),
-            yellow: parseInt(dataForm.yellow.value),
-            black: parseInt(dataForm.black.value),
-            white: parseInt(dataForm.white.value),
-            orange: parseInt(dataForm.orange.value),
-            pink: parseInt(dataForm.pink.value),
-            brown: parseInt(dataForm.brown.value),
-            grey: parseInt(dataForm.grey.value),
-            translucent: parseInt(dataForm.translucent.value),
-        },
-        observations: dataForm.observations.value,
-        date: dataForm.date.value,
-        time: dataForm.time.value,
+        point: dataForm1.point.value,
+        latitude: parseFloat(dataForm1.latitude.value),
+        longitude: parseFloat(dataForm1.longitude.value),
+        tideCondition: dataForm1.tideCondition.value,
+        waterCondition: dataForm1.waterCondition.value,
+        date: dataForm1.date.value,
     };
-    dataStorage.push(data);
+    dataStorage1.push(data);
 
-    displayData();
+    let marker = L.marker([data.latitude, data.longitude]).addTo(markerGroup);
+    marker.bindPopup(`Ponto: ${data.point}<br>Latitude: ${data.latitude}<br>Longitude: ${data.longitude}`).openPopup();
 }
 
-function displayData() {
-    let dataDisplay = document.getElementById('dataDisplay');
-    dataDisplay.innerHTML = '';
-    dataStorage.forEach((data, index) => {
-        let dataItem = document.createElement('div');
-        dataItem.className = 'dataItem';
-        dataItem.innerText = `Ponto: ${data.point}, Latitude: ${data.latitude}, Longitude: ${data.longitude}, Quantidade de Fragmentos: ${data.fragmentQuantity}, Quantidade de Fibras: ${data.fiberQuantity}`;
-        dataDisplay.appendChild(dataItem);
-    });
-}
-
-function downloadData() {
-    let blob = new Blob([JSON.stringify(dataStorage)], { type: 'application/json' });
-    saveAs(blob, 'data.json');
-}
-
-function generateChart() {
-    let chartType = document.getElementById('chartType').value;
-    let dataToPlot = document.getElementById('dataToPlot').value;
-
-    let labels = [];
-    let data = [];
-
-    if (dataToPlot === 'fragmentQuantity' || dataToPlot === 'fiberQuantity' || dataToPlot === 'total') {
-        dataStorage.forEach((item, index) => {
-            labels.push('Ponto ' + (index + 1));
-            if (dataToPlot === 'fragmentQuantity') {
-                data.push(item.fragmentQuantity);
-            } else if (dataToPlot === 'fiberQuantity') {
-                data.push(item.fiberQuantity);
-            } else {
-                data.push(item.fragmentQuantity + item.fiberQuantity);
-            }
-        });
-    } else {
-        let colorData = {
-            blue: 0,
-            red: 0,
-            green: 0,
-            yellow: 0,
-            black: 0,
-            white: 0,
-            orange: 0,
-            pink: 0,
-            brown: 0,
-            grey: 0,
-            translucent: 0,
-        };
-        dataStorage.forEach(item => {
-            for (let color in colorData) {
-                colorData[color] += item.colors[color];
-            }
-        });
-        labels = Object.keys(colorData);
-        data = Object.values(colorData);
-    }
-
-    let ctx = document.getElementById('dataChart').getContext('2d');
-    new Chart(ctx, {
-        type: chartType,
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Dados de Coleta',
-                data: data,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-}
-
-function downloadChart() {
-    let link = document.createElement('a');
-    link.href = document.getElementById('dataChart').toDataURL();
-    link.download = 'chart.png';
-    link.click();
-}
+window.onload = initMap;
